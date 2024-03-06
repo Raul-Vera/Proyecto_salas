c7fd899f11e221e2cf368d2f141a86964ede6638
Raúl Vera <rauluvepe@gmail.com>
2024-03-06 11:29:58 +0100

    Menu hecho,reserva hecho

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int eleccion;
+        boolean opcioninvalida=false;
+        Scanner in=new Scanner(System.in);
+        //Menu
+        do {
+            opcioninvalida=false;
+            System.out.println("----MENU----");
+            System.out.println("1) Login como Administrador");
+            System.out.println("2) Login como Departamento");
+            System.out.println("3) Salir");
+            eleccion=Integer.parseInt(in.nextLine());
 
+            //Menu Administrador
+            if(eleccion==1){
+
+            }
+            if (eleccion<1 ||eleccion>3){
+                System.out.println("Debes introducir una opción valida");
+                opcioninvalida=true;
+            }
+
+        }
+        while (eleccion==1 || eleccion==2||opcioninvalida);
+        System.out.println("Hasta la próxima");
     }
 }
